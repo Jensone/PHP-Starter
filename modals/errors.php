24a9dc106f9d8f1c39f91700fdca6dfa5756a9cd
Jensone <hello@jensone.com>
2024-12-31 11:20:49 +0100

    Add error and success modals for user feedback on appointment confirmations and error messages in the coaching application

---
+++ modals/errors.php
@@ -0,0 +1,16 @@
+<dialog open>
+    <article>
+        <header>
+            <p>
+                <strong>🚨 Une erreur s'est produite</strong>
+            </p>
+        </header>
+        <ul>
+            <?php
+                foreach($_SESSION['errors'] as $error) {
+                    echo "<li>$error</li>";
+                }
+            ?>
+        </ul>
+    </article>
+</dialog>
---
+++ modals/success.php
@@ -0,0 +1,13 @@
+<dialog open>
+    <article>
+        <header>
+            <p>
+                <strong>💪 Votre RDV est confirmé</strong>
+            </p>
+        </header>
+        <p>
+            Félicitations, vous avancez vers votre objectif de forme. Un mail de confirmation vous a été envoyé à l'adresse renseignée.<br>
+            À bientôt avec Coach Sportif ! 🏋
+        </p>
+    </article>
+</dialog>

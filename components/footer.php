f197970620d83a1b919949e2e5391f1e19294136
Jensone <hello@jensone.com>
2024-12-31 15:32:03 +0100

    Add JavaScript to footer for dialog interaction, closing on click and logging to console

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,11 @@
+<script>
+    const dialog = document.querySelector('dialog');
+    dialog.addEventListener( 'click', () => {
+        dialog.removeAttribute('open');
+        console.log('bye');
+    });
+</script>
+
 </main>
 </body>
 </html>

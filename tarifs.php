572ccced294a3db279d03840418770c16cba2a3e
Jensone <hello@jensone.com>
2024-12-31 12:58:25 +0100

    Create tarifs.php with initial HTML structure for pricing and scheduling information on coaching services

---
+++ tarifs.php
@@ -0,0 +1,34 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coach Sportif - Tarifs</title>
+    <link rel="shortcut icon" href="https://api.iconify.design/material-symbols:sports-handball.svg?color=%23000000" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+</head>
+<body>
+    <main class="container">    
+        <h1>Mes tarifs et horaires</h1>
+        <p>Bienvenue sur ma page de prise de RDV</p>
+
+        <article>
+            <header>
+                <p align="center">
+                    Remplissez le formulaire ci-dessous pour réserver votre séance de sport
+                </p>
+            </header>
+            
+            <footer>
+                <p align="center">
+                    Une validation de votre séance sera envoyée à votre adresse email
+                </p>
+            </footer>
+        </article>
+    </main>
+</body>
+</html>
